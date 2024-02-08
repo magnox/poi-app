d0eb02695a273c76efc33544dbacac8fb4c6059e
Max Heimer <max.heimer@7p-group.com>
2024-02-08 10:43:12 +0100

    add chips for filtering

--- poi-app/src/components/PoiFilter.vue
+++ poi-app/src/components/PoiFilter.vue
@@ -1,14 +1,13 @@
 <template>
-    <div class="poi-filter">
-      <h3>Filter POIs</h3>
-      <div v-for="type in poiTypes" :key="type" class="filter-option">
-        <input type="checkbox" :id="type" :value="type" v-model="selectedTypes">
-        <label :for="type">{{ type }}</label>
-      </div>
+  <div class="poi-filter">
+    <div class="chip" v-for="type in poiTypes" :key="type" :class="{ active: selectedTypes.includes(type) }"
+      @click="toggleSelection(type)">
+      {{ type }}
     </div>
-  </template>
+  </div>
+</template>
   
-  <script>
+<script>
 export default {
   name: 'PoiFilter',
   props: {
@@ -25,6 +24,17 @@ export default {
   created() {
     this.selectedTypes = [...this.poiTypes];
   },
+  methods: {
+    toggleSelection(type) {
+      const index = this.selectedTypes.indexOf(type);
+      if (index > -1) {
+        this.selectedTypes.splice(index, 1); // Entferne Typ, wenn bereits ausgewählt
+      } else {
+        this.selectedTypes.push(type); // Füge Typ hinzu, wenn nicht ausgewählt
+      }
+      this.$emit('update-filter', this.selectedTypes);
+    },
+  },
   watch: {
     poiTypes(newVal) {
       this.selectedTypes = [...newVal];
@@ -38,12 +48,22 @@ export default {
 
 
   
-  <style scoped>
-  .poi-filter {
-    /* Stilisierung der Filterkomponente */
-  }
-  .filter-option {
-    /* Stilisierung der einzelnen Filteroptionen */
-  }
-  </style>
+<style scoped>
+.chip {
+  display: inline-block;
+  padding: 5px 10px;
+  border: 1px solid #ccc;
+  border-radius: 25px;
+  margin: 2px;
+  cursor: pointer;
+  transition: background-color 0.3s, color 0.3s;
+  font-family: Sans-Serif;
+}
+
+.chip.active {
+  background-color: #007BFF;
+  color: white;
+  border-color: #007BFF;
+}
+</style>
   
